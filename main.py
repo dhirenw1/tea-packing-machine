d58d349dc7a788eb0bd7b6a6dd2f29e1e048663d
Dhiren Wijesinghe <dhirenw1@gmail.com>
2024-08-19 04:00:34 +0100

    multithreading working for pusher and slider

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 import math
 import time
 from RPi import GPIO           # Allows us to call our GPIO pins and names it just GPIO
-import multiprocessing
+import threading
 
 from pusher_state_machine import main as pusher_sm
 
-pusher_proc = multiprocessing.Process(target=pusher_sm)
+slider_homed = threading.Event()
+push_slider = threading.Event()
+
+def test(slider_homed, push_slider):
+    slider_homed_var = True
+    while True:
+        if push_slider.is_set():
+                print("Pushing slider...")
+                push_slider.clear()
+                time.sleep(2)
+                slider_homed_var = False
+                slider_homed.clear()
+                print("HOMING SLIDER...")
+                time.sleep(5)
+                slider_homed.set()
+                slider_homed_var = True
+
+
+
+pusher_thread = threading.Thread(target=pusher_sm, args=(slider_homed,push_slider))
+test_thread = threading.Thread(target=test, args=(slider_homed,push_slider))
 
 def execute_app():
-    pusher_proc.start()
+    slider_homed.set()
+    push_slider.clear()
+    pusher_thread.start()
+    test_thread.start()
+
 
 def handle_cleanup():
-    pusher_proc.join()
+    # pusher_process.join()
+    pusher_thread.join()
+    test_thread.join()
+    print("done")
 
 def main():
     try:
+        slider_homed.set()
         execute_app()
     finally:
         handle_cleanup()
--- pusher_state_machine.py
+++ pusher_state_machine.py
@@ -4,7 +4,9 @@ from motor_driver.motor import Motor
 import math
 import time
 from RPi import GPIO           # Allows us to call our GPIO pins and names it just GPIO
- 
+import threading
+import argparse
+
 GPIO.setmode(GPIO.BCM)           # Set's GPIO pins to BCM GPIO numbering
 INPUT_PIN = 4           # Sets our input pin, in this example I'm connecting our button to pin 4. Pin 0 is the SDA pin so I avoid using it for sensors/buttons
 GPIO.setup(INPUT_PIN, GPIO.IN)           # Set our input pin to be an input
@@ -12,26 +14,27 @@ GPIO.setup(INPUT_PIN, GPIO.IN)           # Set our input pin to be an input
 # raspi serial peripheral used by Modbus RTU
 peripheral = '/dev/serial0'
 
-NUM_BAGS_PER_COL = 30
+NUM_BAGS_PER_COL = 20
 NUM_BAGS_IN_BUFFER = 10
 
 START_POS = 6
 PUSHED_POS = 10
+STAGE_POS = 10*math.pi
 
 m1 = Motor(peripheral, 1, 115200, setVel=1400, setAcc=25, setDec=25)
 m1.set_abs_position(0)
 
-def execute_app():
+def execute_app(slider_homed, push_slider):
     bag = 0
     # GPIO.add_event_detect(INPUT_PIN, GPIO.FALLING, bouncetime=1)
     while True:
-
         # if GPIO.event_detected(INPUT_PIN):
         # print(m1.get_read_digital_inputs())
         if m1.get_read_digital_inputs() & 0x4 == 0x0:
             print(bag)
             if bag < NUM_BAGS_PER_COL:
-                if bag < NUM_BAGS_IN_BUFFER:
+                if bag < NUM_BAGS_IN_BUFFER and slider_homed.is_set() is False:
+                    print("Not homed")
                     m1.set_rel_position(PUSHED_POS - 0.4*bag)
                     while m1.motor_command_done() is not True:
                         pass
@@ -39,6 +42,7 @@ def execute_app():
                     while m1.motor_command_done() is not True:
                         pass
                 else:
+                    print("homed")
                     m1.set_rel_position(PUSHED_POS)
                     while m1.motor_command_done() is not True:
                         pass
@@ -48,6 +52,17 @@ def execute_app():
                 bag += 1
             else:
                 bag = 0
+
+                # Push slider to move column into stager
+                m1.set_abs_position(STAGE_POS)
+                while m1.motor_command_done() is not True:
+                    pass
+                print("UNSTAGING")
+                m1.set_abs_position(START_POS)
+                while m1.motor_command_done() is not True:
+                    pass
+                push_slider.set()
+
             while m1.get_read_digital_inputs() & 0x4 == 0:
                 pass
 
@@ -56,11 +71,18 @@ def execute_app():
 def handle_cleanup():
     m1.stop()
 
-def main():
+def main(slider_homed, push_slider):
     try:
-        execute_app()
+        execute_app(slider_homed, push_slider)
     finally:
         handle_cleanup()
 
-if __name__=='__main__':
-    main()
+# if __name__=='__main__':
+    # execute_app()
+
+#     parser = argparse.ArgumentParser(description='pusher state machine')
+#     parser.add_argument('--event', metavar='event', required=True,
+#                         help='slider event')
+#     args = parser.parse_args()
+
+#     main(args.event)
