83eb2883becd738e1b23c88db864017199057da1
Dhiren Wijesinghe <dhirenw1@gmail.com>
2024-08-21 04:15:36 +0100

    code cleanup

--- main.py
+++ main.py
@@ -19,28 +19,17 @@ stager_process = multiprocessing.Process(target=stager, args=(load_bags_event,))
 test_proc = multiprocessing.Process(target=print_msg, args=(load_bags_event,))
 
 def execute_app():
-#     slider_homed.set()
-#     push_slider.clear()
-#     pusher_thread.start()
-#     test_thread.start()
     stager_process.start()
     test_proc.start()
     while True:
         pass
 
-
 def handle_cleanup():
-#     # pusher_process.join()
-#     pusher_thread.join()
-#     test_thread.join()
-#     print("done")
     stager_process.join()
     test_proc.join()
 
 def main():
     execute_app()
-
-
 
 if __name__=='__main__':
     main()
